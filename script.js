cc6bd8ab1e4bcf0a9b6279408b47dce18eea0ceb
angelmusonda <angelmusonda9@gmail.com>
2024-02-04 16:02:07 +0200

    new file:   construction.html 	new file:   design.html 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ function openMoreAboutFeasibility() {
     window.location.href = targetPageURL;
 }
 
+function openMoreAboutDesign() {
+    // Specify the relative or absolute URL of the target page
+    var targetPageURL = 'design.html';
+
+    // Navigate to the target page
+    window.location.href = targetPageURL;
+}
+
+function openMoreAboutConstruction() {
+    // Specify the relative or absolute URL of the target page
+    var targetPageURL = 'construction.html';
+
+    // Navigate to the target page
+    window.location.href = targetPageURL;
+}
 function openHome() {
     // Specify the relative or absolute URL of the target page
     var targetPageURL = 'index.html';
