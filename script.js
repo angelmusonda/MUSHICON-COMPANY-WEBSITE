17ddfbb92b8a55eb6324d85e2f4aff1687026646
angelmusonda <angelmusonda9@gmail.com>
2024-02-04 13:27:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function openMoreAboutFeasibility() {
+    // Specify the relative or absolute URL of the target page
+    var targetPageURL = 'feasibility.html';
+
+    // Navigate to the target page
+    window.location.href = targetPageURL;
+}
+
+function openHome() {
+    // Specify the relative or absolute URL of the target page
+    var targetPageURL = 'home.html';
+
+    // Navigate to the target page
+    window.location.href = targetPageURL;
+}
+
+
